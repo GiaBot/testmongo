af699606d238e3430766a191aa77d833e1335e80
Gia Huynh <tuonghuynh256@gmail.com>
2023-01-24 09:52:30 +0100

    Changed schemas and few Repositories

--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -1,8 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationUpdate;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +28,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Repository.OrdersRepository;
 import com.example.demo.models.Orders;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+import ch.qos.logback.core.model.Model;
 
 @RestController
 @RequestMapping("/api")
 public class OrdersController {
     
     final private OrdersRepository repository;
+    private MongoClient client = MongoClients.create(MongoClientSettings.builder()
+    .applyConnectionString(new ConnectionString("mongodb://mongoadmin:mongoadmin@localhost:27017/"))
+    .build());
+    private MongoDatabase database = client.getDatabase("Test");
+    private MongoCollection<Document> orders = database.getCollection("orders");
+    private MongoTemplate mongoTemplate = new MongoTemplate(client, "Test");
 
     public OrdersController(OrdersRepository repository) {
         this.repository = repository;
@@ -84,6 +116,65 @@ public class OrdersController {
     @PutMapping("saveManyOrders")
     public List<Orders> saveManyOrders(@RequestBody List<Orders> orders) {
         return repository.saveAll(orders);
+    }
+
+    @PutMapping("changeAmount/{id}")
+    public void changeAmount(@PathVariable String id, @RequestParam int amount, @RequestParam String modellId) {
+
+    }
+
+    @PutMapping("updateTotalPrice/{id}")
+    public void updateTotalPriceById(@PathVariable String id) {
+        // float totalPrice = orders.get(0).getTotalPrice();
+        // orders.get(0).setTotalPrice(totalPrice * amount);
+        // repository.save(orders.get(0));
+
+        // AggregationUpdate update = Aggregation.newUpdate()
+        //     .set("price")
+        //     .toValue(ArithmeticOperators.valueOf("price")
+        //     .multiplyBy(amount));
+
+        // mongoTemplate.update(Orders.class)
+        //     .apply(update)
+        //     .all();
+        AggregationUpdate update = Aggregation.newUpdate()
+            .set("totalPrice")
+            .toValue(ArithmeticOperators.valueOf("modells.price")
+            .multiplyBy("modells.amount"));
+
+    }
+
+    public void updateTotalPrice(String id) {
+        // String[] keys;
+        // Integer[] values;
+        // Query query = new Query();
+        // query.addCriteria(Criteria.where("_id").is(id));
+        // ArrayList<Bson> updateList = new ArrayList<>();
+        // Orders order = mongoTemplate.findOne(query, Orders.class);
+        // if (order != null) {
+        //     values = (Integer[]) modells.values().toArray();
+        //     for (Integer integer : values) {
+        //         Bson updates = Updates.mul("totalPrice", integer);
+        //         updateList.add(updates);
+        //     }
+        //     models.updateMany(Filters.in("_id", modells.keySet()), updateList);
+        // }
+
+
+        
+        AggregationUpdate update = Aggregation.newUpdate()
+            .set("totalPrice")
+            .toValue(ArithmeticOperators.valueOf("price")
+            .multiplyBy("amount"));
+
+        orders.updateMany(new Document(),
+                new Document("$set",
+                new Document("totalPrice", update)));
+
+    }
+
+    public void updateOnPrice() {
+
     }
 
 }
